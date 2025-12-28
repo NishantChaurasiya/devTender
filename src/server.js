4b825c409318c72d5d88fde5bbcdd18b98fee710
nishantchaurasiya <nishantchaurasiya1108@gmail.com>
2025-12-28 10:55:18 +0530

    explore query and params

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 const express=require("express");
 const app=express()
-app.get("/test",(req,res)=>{
-    res.send("hello from test page")
+
+app.get("/user",(req,res)=>{
+ console.log(req.query);
+ res.send("done")
+ 
 })
-app.post("/user",(req,res)=>{
-    res.send("data save in database")
-})
-app.get("/",(req,res)=>{
-    res.send("hello from home page")
+app.get("/product/:id/:name/:price",(req,res)=>{
+console.log(req.params);
+
+res.send("fetching data done")
 })
 app.listen(3000,()=>{
     console.log("server in running on port 3000");
